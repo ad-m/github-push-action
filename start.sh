fbe6aa5e4bee71fee11c3e8975233985d9babd12
Adam Dobrawy <naczelnik@jawnosc.tk>
2019-08-24 13:44:51 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-set -eux
 echo "Push to branch ${INPUT_BRANCH}";
+[ -z "${INPUT_GITHUB_TOKEN}" ] && {
+    echo 'Missing input "github_token: ${{ secrets.GITHUB_TOKEN }}".';
+    exit 1;
+};
 
 header=$(echo -n "ad-m:${INPUT_GITHUB_TOKEN}" | base64)
 git -c http.extraheader="AUTHORIZATION: basic $header" push origin refs/remotes/origin/master:master;
