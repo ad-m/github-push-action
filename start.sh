af9fa9e17f6a09ec8b0e93240b3114fdde096073
Adam Dobrawy <naczelnik@jawnosc.tk>
2019-08-24 13:34:41 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eux
-echo "Push to branch $BRANCH";
+echo "Push to branch ${INPUT_BRANCH}";
 
 header=$(echo -n "ad-m:${INPUT_REPO-TOKEN}" | base64)
 git -c http.extraheader="AUTHORIZATION: basic $header" push origin refs/remotes/origin/master:master;
