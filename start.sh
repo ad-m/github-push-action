e8d06b80faa350087ed0cbcc9f9cfee9644a3557
Adam Dobrawy <ad-m@users.noreply.github.com>
2019-09-12 03:06:46 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-echo "Push to branch ${INPUT_BRANCH}";
+echo "Push to branch ${INPUT_BRANCH:=master}";
 [ -z "${INPUT_GITHUB_TOKEN}" ] && {
     echo 'Missing input "github_token: ${{ secrets.GITHUB_TOKEN }}".';
     exit 1;
 };
 
 header=$(echo -n "ad-m:${INPUT_GITHUB_TOKEN}" | base64)
-git -c http.extraheader="AUTHORIZATION: basic $header" push origin HEAD:master;
+git -c http.extraheader="AUTHORIZATION: basic $header" push origin HEAD:${INPUT_BRANCH};
