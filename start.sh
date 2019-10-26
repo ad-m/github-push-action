c55b1c209b5507d6b506bf2d1d2abb103147b2e7
anarcher <anarcher@gmail.com>
2019-10-27 01:41:53 +0900

    Fix to use REPOSITORY

--- start.sh
+++ start.sh
@@ -19,6 +19,6 @@ fi
 
 cd ${INPUT_DIRECTORY}
 
-remote_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+remote_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${REPOSITORY}.git"
 
 git push "${remote_repo}" HEAD:${INPUT_BRANCH} --follow-tags $_FORCE_OPTION;
