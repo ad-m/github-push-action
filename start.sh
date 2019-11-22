508672e3d661a58b39143a7b54bea3bddf0f931b
Santiago Fraire Willemoës <santiwilly@gmail.com>
2019-11-22 12:30:46 +0100

    feat: tags is now configurable

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@ set -e
 
 INPUT_BRANCH=${INPUT_BRANCH:-master}
 INPUT_FORCE=${INPUT_FORCE:-false}
+INPUT_TAGS=${INPUT_TAGS:-false}
 INPUT_DIRECTORY=${INPUT_DIRECTORY:-'.'}
 _FORCE_OPTION=''
 REPOSITORY=${INPUT_REPOSITORY:-$GITHUB_REPOSITORY}
@@ -17,8 +18,12 @@ if ${INPUT_FORCE}; then
     _FORCE_OPTION='--force'
 fi
 
+if ${TAGS}; then
+    _TAGS='--tags'
+fi
+
 cd ${INPUT_DIRECTORY}
 
 remote_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${REPOSITORY}.git"
 
-git push "${remote_repo}" HEAD:${INPUT_BRANCH} --follow-tags $_FORCE_OPTION --tags;
+git push "${remote_repo}" HEAD:${INPUT_BRANCH} --follow-tags $_FORCE_OPTION $_TAGS;
