1bf41a92a1d1808480fea85dcc503e02e0952947
Adam Dobrawy <ad-m@users.noreply.github.com>
2021-01-02 06:50:13 +0100

    Revert "refactor(start): use @actions/exec (#47)" (#73)
    This reverts commit c52e809b5674f36e38bf83f07568dbf04f7921d4.

--- start.js
+++ start.js
@@ -1,12 +1,26 @@
-const exec = require("@actions/exec");
+const spawn = require('child_process').spawn;
 const path = require("path");
 
+const exec = (cmd, args=[]) => new Promise((resolve, reject) => {
+    console.log(`Started: ${cmd} ${args.join(" ")}`)
+    const app = spawn(cmd, args, { stdio: 'inherit' });
+    app.on('close', code => {
+        if(code !== 0){
+            err = new Error(`Invalid status code: ${code}`);
+            err.code = code;
+            return reject(err);
+        };
+        return resolve(code);
+    });
+    app.on('error', reject);
+});
+
 const main = async () => {
-  await exec(path.join(__dirname, "./start.sh"));
+    await exec('bash', [path.join(__dirname, './start.sh')]);
 };
 
 main().catch(err => {
-  console.error(err);
-  console.error(err.stack);
-  process.exit(err.code || -1);
-});
+    console.error(err);
+    console.error(err.stack);
+    process.exit(err.code || -1);
+})
