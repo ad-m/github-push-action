c52e809b5674f36e38bf83f07568dbf04f7921d4
jjangga0214 <jjangga@kookmin.ac.kr>
2021-01-02 14:29:28 +0900

    refactor(start): use @actions/exec (#47)

--- start.js
+++ start.js
@@ -1,26 +1,12 @@
-const spawn = require('child_process').spawn;
+const exec = require("@actions/exec");
 const path = require("path");
 
-const exec = (cmd, args=[]) => new Promise((resolve, reject) => {
-    console.log(`Started: ${cmd} ${args.join(" ")}`)
-    const app = spawn(cmd, args, { stdio: 'inherit' });
-    app.on('close', code => {
-        if(code !== 0){
-            err = new Error(`Invalid status code: ${code}`);
-            err.code = code;
-            return reject(err);
-        };
-        return resolve(code);
-    });
-    app.on('error', reject);
-});
-
 const main = async () => {
-    await exec('bash', [path.join(__dirname, './start.sh')]);
+  await exec(path.join(__dirname, "./start.sh"));
 };
 
 main().catch(err => {
-    console.error(err);
-    console.error(err.stack);
-    process.exit(err.code || -1);
-})
+  console.error(err);
+  console.error(err.stack);
+  process.exit(err.code || -1);
+});
