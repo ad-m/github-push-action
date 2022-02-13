f030f995a2f3fc0dfd5fdd0ca79c71484447d080
Pascal Zimmermann <Z_pascal@yahoo.de>
2022-02-13 15:47:06 +0100

    * Add GitHub url and GitHub Enterprise support * Update the GitHub action

--- start.sh
+++ start.sh
@@ -23,6 +23,10 @@ fi
 
 cd ${INPUT_DIRECTORY}
 
-remote_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${REPOSITORY}.git"
+remote_repo="${INPUT_GITHUB_URL_PROTOCOL}//${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@${INPUT_GITHUB_URL}/${REPOSITORY}.git"
+if ${INPUT_TEST}; then
+  exit 0
+else:
+  git push "${remote_repo}" HEAD:${INPUT_BRANCH} --follow-tags $_FORCE_OPTION $_TAGS;
+fi
 
-git push "${remote_repo}" HEAD:${INPUT_BRANCH} --follow-tags $_FORCE_OPTION $_TAGS;
